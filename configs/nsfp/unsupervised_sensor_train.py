01b39b4c93d964fe781b61ef5d20fa483366eb1b
Kyle Vedder <kyle.c.vedder@gmail.com>
2023-02-02 18:32:35 -0500

    Added NSFP sensor train split

--- configs/nsfp/unsupervised_sensor_train.py
+++ configs/nsfp/unsupervised_sensor_train.py
@@ -1,8 +1,9 @@
+is_trainable = False
+has_labels = False
 
-test_sequence_dir = "/efs/argoverse2/val/"
-test_flow_dir = "/efs/argoverse2/val_sceneflow/"
 
-flow_save_folder = "/efs/argoverse_lidar/train_flow/"
+test_sequence_dir = "/efs/argoverse2/train/"
+flow_save_folder = "/efs/argoverse2/train_nsfp_flow/"
 
 precision = 32
 
@@ -23,22 +24,19 @@ validate_every = 500
 
 SEQUENCE_LENGTH = 2
 
-is_trainable = False
-
 model = dict(name="NSFP",
              args=dict(VOXEL_SIZE=(0.2, 0.2, 4),
                        POINT_CLOUD_RANGE=(-51.2, -51.2, -3, 51.2, 51.2, 1),
                        SEQUENCE_LENGTH=SEQUENCE_LENGTH,
                        flow_save_folder=flow_save_folder))
 
-test_loader = dict(name="ArgoverseFlowSequenceLoader",
-                   args=dict(raw_data_path=test_sequence_dir,
-                             flow_data_path=test_flow_dir))
+test_loader = dict(name="ArgoverseSequenceLoader",
+                   args=dict(sequence_dir=test_sequence_dir))
 
 test_dataloader = dict(
     args=dict(batch_size=1, num_workers=1, shuffle=False, pin_memory=True))
 
-test_dataset = dict(name="SubsequenceFlowDataset",
+test_dataset = dict(name="SubsequenceDataset",
                     args=dict(subsequence_length=SEQUENCE_LENGTH,
                               max_sequence_length=max_test_sequence_length,
                               origin_mode="FIRST_ENTRY"))
