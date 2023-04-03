fbeb5eded83cce802e46124fc2a986a9f06dbdc0
Kyle Vedder <kyle.c.vedder@gmail.com>
2023-04-03 14:18:29 -0400

    Added flow check

--- check_waymo_data.py
+++ check_waymo_data.py
@@ -10,14 +10,26 @@ args = parser.parse_args()
 training_dir = args.waymo_dir / 'training'
 validation_dir = args.waymo_dir / 'validation'
 
+flow_label_dir = args.waymo_dir / 'train_nsfp_flow'
+
 assert training_dir.exists(), 'Training directory does not exist'
 assert validation_dir.exists(), 'Validation directory does not exist'
 
 training_subfolders = sorted(training_dir.glob('segment*'))
 validation_subfolders = sorted(validation_dir.glob('segment*'))
 
-assert len(training_subfolders) == 798, f'Expected 798 training subfolders, got {len(training_subfolders)}'
-assert len(validation_subfolders) == 202, f'Expected 202 validation subfolders, got {len(validation_subfolders)}'
+flow_subfolders = sorted(flow_label_dir.glob('segment*'))
+
+assert len(
+    training_subfolders
+) == 798, f'Expected 798 training subfolders, got {len(training_subfolders)}'
+assert len(
+    validation_subfolders
+) == 202, f'Expected 202 validation subfolders, got {len(validation_subfolders)}'
+
+assert len(
+    flow_subfolders
+) == 798, f'Expected 798 flow subfolders, got {len(flow_subfolders)}'
 
 # Check that all subfolders have the expected number of flows
 for subfolder in training_subfolders:
@@ -25,4 +37,12 @@ for subfolder in training_subfolders:
     if len(num_flows) < 196:
         print(str(subfolder), len(num_flows))
 
-    
+for subfolder in validation_subfolders:
+    num_flows = sorted(subfolder.glob('*.pkl'))
+    if len(num_flows) < 196:
+        print(str(subfolder), len(num_flows))
+
+for subfolder in flow_subfolders:
+    num_flows = sorted(subfolder.glob('*.npz'))
+    if len(num_flows) < 196:
+        print(str(subfolder), len(num_flows))
