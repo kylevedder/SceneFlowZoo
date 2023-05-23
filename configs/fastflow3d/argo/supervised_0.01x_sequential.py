7b44dd7d0817605d22461157453bd43e76138e29
Kyle Vedder <kyle.c.vedder@gmail.com>
2023-05-23 13:42:21 -0400

    Moved validation to once at the end

--- configs/fastflow3d/argo/supervised_0.01x_sequential.py
+++ configs/fastflow3d/argo/supervised_0.01x_sequential.py
@@ -1,6 +1,6 @@
 _base_ = "./supervised.py"
 
 epochs = 200
-check_val_every_n_epoch = 5
+check_val_every_n_epoch = 200
 validate_every = None
 dataset = dict(args=dict(subset_fraction=0.01, subset_mode='sequential'))
