e3e72dce7f75dcd02ce5946977ece73b1914919a
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-01-18 12:01:56 -0500

    Added cleaned up vis infra

--- visualization/visualize_flow.py
+++ visualization/visualize_flow.py
@@ -5,12 +5,31 @@ from bucketed_scene_flow_eval.datasets.argoverse2 import ArgoverseSceneFlowSeque
 import numpy as np
 import tqdm
 from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--sequence_id", type=str, default="0b86f508-5df9-4a46-bc59-5b9536dbde9f")
+parser.add_argument("--sequence_start_idx", type=int, default=125)
+# Path to the sequence folder
+parser.add_argument("--sequence_folder", type=str, default="/efs/argoverse2/val/")
+# Path to the flow folder
+parser.add_argument(
+    "--flow_folder",
+    type=str,
+    default="/efs/argoverse2/val_sceneflow_feather/",
+    help="Path to the flow folder. This can be the ground truth flow, or dumped results from a model.",
+)
+args = parser.parse_args()
 
 # sequence_loader = ArgoverseSequenceLoader('/bigdata/argoverse_lidar/train/')
-sequence_loader = ArgoverseSceneFlowSequenceLoader("/efs/argoverse2/val/", use_gt_flow=False)
+sequence_loader = ArgoverseSceneFlowSequenceLoader(
+    args.sequence_folder,
+    args.flow_folder,
+    use_gt_flow=False,
+)
 
 # sequence_id = sequence_loader.get_sequence_ids()[2]
-sequence_id = "0b86f508-5df9-4a46-bc59-5b9536dbde9f"
+sequence_id = args.sequence_id
 print("Sequence ID: ", sequence_id)
 sequence = sequence_loader.load_sequence(sequence_id)
 
@@ -34,7 +53,7 @@ screenshot_path = Path() / "screenshots"
 screenshot_path.mkdir(exist_ok=True)
 draw_flow_lines_color = "green"
 full_frame_list = sequence.load_frame_list(0)
-starter_idx = 125
+starter_idx = args.sequence_start_idx
 
 
 def increase_starter_idx(vis):
@@ -42,7 +61,7 @@ def increase_starter_idx(vis):
     starter_idx += 1
     if starter_idx >= len(full_frame_list) - 1:
         starter_idx = 0
-    print(starter_idx)
+    print("Index: ", starter_idx)
     vis.clear_geometries()
     draw_frames(reset_view=False)
 
@@ -52,7 +71,7 @@ def decrease_starter_idx(vis):
     starter_idx -= 1
     if starter_idx < 0:
         starter_idx = len(full_frame_list) - 2
-    print(starter_idx)
+    print("Index: ", starter_idx)
     vis.clear_geometries()
     draw_frames(reset_view=False)
 
@@ -85,11 +104,18 @@ vis.register_key_callback(263, decrease_starter_idx)
 vis.register_key_callback(262, increase_starter_idx)
 vis.register_key_callback(ord("S"), save_screenshot)
 
+print("#############################################################")
+print("Flow moves from the gray point cloud to the white point cloud\n")
+print("Press F to toggle flow lines")
+print("Press left or right arrow to change starter_idx")
+print(f"Press S to save screenshot (saved to {screenshot_path.absolute()})")
+print("#############################################################")
+
 
 def draw_frames(reset_view=False):
     frame_list = full_frame_list[starter_idx : starter_idx + 2]
     color_scalar = np.linspace(0.5, 1, len(frame_list))
-    for idx, frame_dict in enumerate(tqdm.tqdm(frame_list)):
+    for idx, frame_dict in enumerate(frame_list):
         pc = frame_dict["relative_pc"]
         pose = frame_dict["relative_pose"]
         flowed_pc = frame_dict["relative_flowed_pc"]
@@ -104,8 +130,6 @@ def draw_frames(reset_view=False):
 
         # Add flowed point cloud
         if flowed_pc is not None and idx < len(frame_list) - 1:
-            print("Max flow magnitude:", pc.matched_point_distance(flowed_pc).max())
-
             if draw_flow_lines_color is not None:
                 line_set = o3d.geometry.LineSet()
                 assert len(pc) == len(
