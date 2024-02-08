1dcd078af0013511edf3c04767fdd631bffdd5ad
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-02-08 18:11:27 -0500

    Removed NSFP cached

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,9 @@
 from .fast_flow_3d import FastFlow3D, FastFlow3DBucketedLoaderLoss, FastFlow3DSelfSupervisedLoss
-from .nsfp import NSFP, NSFPCached
+from .nsfp import NSFP
 
 __all__ = [
     "FastFlow3D",
     "FastFlow3DBucketedLoaderLoss",
     "FastFlow3DSelfSupervisedLoss",
     "NSFP",
-    "NSFPCached",
 ]
