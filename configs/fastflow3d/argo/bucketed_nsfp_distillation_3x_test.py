177419415c49088ef30ca6a5dd9b7589842ed6ee
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-02-27 20:22:12 -0500

    Fixed bug with test dataloader trying to load labels

--- configs/fastflow3d/argo/bucketed_nsfp_distillation_3x_test.py
+++ configs/fastflow3d/argo/bucketed_nsfp_distillation_3x_test.py
@@ -2,4 +2,5 @@ _base_ = "./bucketed_nsfp_distillation_1x_test.py"
 
 test_dataset = dict(
     args=dict(
-              eval_args=dict(output_path = "eval_results/bucketed_epe/nsfp_distillation_3x_test/")))
+        load_flow=False,
+        eval_args=dict(output_path = "eval_results/bucketed_epe/nsfp_distillation_3x_test/")))
--- configs/fastflow3d/argo/bucketed_supervised_test.py
+++ configs/fastflow3d/argo/bucketed_supervised_test.py
@@ -7,6 +7,7 @@ test_dataset_root = "/efs/argoverse2/test/"
 test_dataset = dict(
     args=dict(
         root_dir=test_dataset_root,
+        load_flow=False,
         eval_args=dict(output_path="eval_results/bucketed_epe/supervised_test/"),
     )
 )
