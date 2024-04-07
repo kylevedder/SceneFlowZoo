6d445584d98bea733062df5e76769475e4005c58
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-04-07 16:52:26 -0400

    Added subset splitting behavior directly to the dataloader frontend

--- dataloaders/scene_trajectory_benchmark_scene_flow_dataset.py
+++ dataloaders/scene_trajectory_benchmark_scene_flow_dataset.py
@@ -2,13 +2,14 @@ from abc import ABC, abstractmethod
 
 from .dataclasses import BucketedSceneFlowInputSequence, BucketedSceneFlowOutputSequence
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import numpy as np
 import torch
 from bucketed_scene_flow_eval.datasets import construct_dataset
 from bucketed_scene_flow_eval.datastructures import SE3, TimeSyncedSceneFlowFrame, EgoLidarFlow
 from bucketed_scene_flow_eval.interfaces import LoaderType, AbstractDataset
+from dataclasses import dataclass
 
 
 class EvalWrapper:
@@ -73,6 +74,40 @@ class EvalWrapper:
         return self.evaluator.compute_results(save_results=save_results)
 
 
+@dataclass
+class BucketedSceneFlowDatasetSplit:
+    split_idx: int
+    num_splits: int
+
+    def __post_init__(self):
+        # Type check the split index and number of splits.
+        assert isinstance(self.split_idx, int), f"Invalid split index type {type(self.split_idx)}."
+        assert isinstance(
+            self.num_splits, int
+        ), f"Invalid number of splits type {type(self.num_splits)}."
+        assert (
+            0 <= self.split_idx < self.num_splits
+        ), f"Invalid split index {self.split_idx} for {self.num_splits} splits."
+
+    def current_split_length(self, total_length: int) -> int:
+        # All splits should be the same base length but
+        # the remainder should be distributed across the splits.
+        base_length = total_length // self.num_splits
+        distributed_remainder = 1 if total_length % self.num_splits > self.split_idx else 0
+        return base_length + distributed_remainder
+
+    def _current_split_global_start(self, total_length: int) -> int:
+        base_length = total_length // self.num_splits
+        distributed_remainder = min(total_length % self.num_splits, self.split_idx)
+        return base_length * self.split_idx + distributed_remainder
+
+    def split_index_to_global_index(self, split_idx: int, global_length: int) -> int:
+        assert (
+            0 <= split_idx < self.current_split_length(global_length)
+        ), f"Invalid split index {split_idx}."
+        return self._current_split_global_start(global_length) + split_idx
+
+
 class BucketedSceneFlowDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -80,16 +115,28 @@ class BucketedSceneFlowDataset(torch.utils.data.Dataset):
         root_dir: Path,
         max_pc_points: int = 120000,
         set_length: Optional[int] = None,
+        split: Union[BucketedSceneFlowDatasetSplit, dict[str, int]] = BucketedSceneFlowDatasetSplit(
+            0, 1
+        ),
         **kwargs,
     ):
         self.dataset = construct_dataset(dataset_name, dict(root_dir=root_dir, **kwargs))
         self.max_pc_points = max_pc_points
         self.set_length = set_length
+        self.split = (
+            split
+            if isinstance(split, BucketedSceneFlowDatasetSplit)
+            else BucketedSceneFlowDatasetSplit(**split)
+        )
+
+    def _global_len(self):
+        global_length = len(self.dataset)
+        if self.set_length is not None:
+            global_length = min(global_length, self.set_length)
+        return global_length
 
     def __len__(self):
-        if self.set_length is not None:
-            return self.set_length
-        return len(self.dataset)
+        return self.split.current_split_length(self._global_len())
 
     def evaluator(self) -> EvalWrapper:
         return EvalWrapper(self.dataset)
@@ -99,12 +146,14 @@ class BucketedSceneFlowDataset(torch.utils.data.Dataset):
     ) -> list[BucketedSceneFlowInputSequence]:
         return batch
 
-    def __getitem__(self, idx) -> BucketedSceneFlowInputSequence:
-        assert isinstance(idx, int), f"Index must be an integer. Got {type(idx)}."
-        assert 0 <= idx < len(self), "Index out of range."
-        frame_list = self.dataset[idx]
+    def __getitem__(self, split_idx) -> BucketedSceneFlowInputSequence:
+        assert isinstance(split_idx, int), f"Index must be an integer. Got {type(split_idx)}."
+        assert 0 <= split_idx < len(self), "Index out of range."
+
+        global_idx = self.split.split_index_to_global_index(split_idx, self._global_len())
+        frame_list = self.dataset[global_idx]
         return BucketedSceneFlowInputSequence.from_frame_list(
-            idx,
+            global_idx,
             frame_list,
             pc_max_len=self.max_pc_points,
             loader_type=self.dataset.loader_type(),
---
+++ tests/unit_tests/dataset_len_tests.py
@@ -0,0 +1,119 @@
+import pytest
+from dataloaders import BucketedSceneFlowDataset, BucketedSceneFlowInputSequence
+from pathlib import Path
+import torch
+
+
+@pytest.fixture
+def first_split_dataset() -> BucketedSceneFlowDataset:
+    dataset = BucketedSceneFlowDataset(
+        dataset_name="Argoverse2CausalSceneFlow",
+        root_dir=Path("/tmp/argoverse2_small/val/"),
+        flow_data_path=Path("/tmp/argoverse2_small/val_sceneflow_feather/"),
+        subsequence_length=4,
+        split=dict(split_idx=0, num_splits=3),
+    )
+
+    return dataset
+
+
+@pytest.fixture
+def second_split_dataset() -> BucketedSceneFlowDataset:
+    dataset = BucketedSceneFlowDataset(
+        dataset_name="Argoverse2CausalSceneFlow",
+        root_dir=Path("/tmp/argoverse2_small/val/"),
+        flow_data_path=Path("/tmp/argoverse2_small/val_sceneflow_feather/"),
+        subsequence_length=4,
+        split=dict(split_idx=1, num_splits=3),
+    )
+
+    return dataset
+
+
+@pytest.fixture
+def third_split_dataset() -> BucketedSceneFlowDataset:
+    dataset = BucketedSceneFlowDataset(
+        dataset_name="Argoverse2CausalSceneFlow",
+        root_dir=Path("/tmp/argoverse2_small/val/"),
+        flow_data_path=Path("/tmp/argoverse2_small/val_sceneflow_feather/"),
+        subsequence_length=4,
+        split=dict(split_idx=2, num_splits=3),
+    )
+
+    return dataset
+
+
+@pytest.fixture
+def full_dataset() -> BucketedSceneFlowDataset:
+    dataset = BucketedSceneFlowDataset(
+        dataset_name="Argoverse2CausalSceneFlow",
+        root_dir=Path("/tmp/argoverse2_small/val/"),
+        flow_data_path=Path("/tmp/argoverse2_small/val_sceneflow_feather/"),
+        subsequence_length=4,
+    )
+
+    return dataset
+
+
+def test_load_first_split_datset(first_split_dataset: BucketedSceneFlowDataset):
+    assert (
+        len(first_split_dataset) == 104
+    ), f"Expected 104 sequences, but got {len(first_split_dataset)}."
+
+
+def test_load_second_split_datset(second_split_dataset: BucketedSceneFlowDataset):
+    assert (
+        len(second_split_dataset) == 103
+    ), f"Expected 103 sequences, but got {len(second_split_dataset)}."
+
+
+def test_load_third_split_datset(third_split_dataset: BucketedSceneFlowDataset):
+    assert (
+        len(third_split_dataset) == 103
+    ), f"Expected 103 sequences, but got {len(third_split_dataset)}."
+
+
+def test_load_full_datset(full_dataset: BucketedSceneFlowDataset):
+    assert len(full_dataset) == 310, f"Expected 310 sequences, but got {len(full_dataset)}."
+
+
+def test_load_full_and_first_split_get_same_elements(
+    full_dataset: BucketedSceneFlowDataset, first_split_dataset: BucketedSceneFlowDataset
+):
+    full_elem = full_dataset[0]
+    split_elem = first_split_dataset[0]
+
+    assert len(full_elem) == len(
+        split_elem
+    ), f"Expected same length, but got {len(full_elem)} and {len(split_elem)}."
+
+    for idx in range(len(full_elem)):
+        assert torch.all(full_elem.get_full_global_pc(idx) == split_elem.get_full_global_pc(idx))
+
+
+def test_load_full_and_second_split_get_same_elements(
+    full_dataset: BucketedSceneFlowDataset, second_split_dataset: BucketedSceneFlowDataset
+):
+    full_elem = full_dataset[104]  # length of first split
+    split_elem = second_split_dataset[0]
+
+    assert len(full_elem) == len(
+        split_elem
+    ), f"Expected same length, but got {len(full_elem)} and {len(split_elem)}."
+
+    for idx in range(len(full_elem)):
+        assert torch.all(full_elem.get_full_global_pc(idx) == split_elem.get_full_global_pc(idx))
+
+
+def test_load_full_and_third_split_get_same_elements(
+    full_dataset: BucketedSceneFlowDataset, third_split_dataset: BucketedSceneFlowDataset
+):
+    full_elem = full_dataset[207]  # length of the first + second split, 104 + 103
+    split_elem = third_split_dataset[0]
+
+    assert len(full_elem) == len(
+        split_elem
+    ), f"Expected same length, but got {len(full_elem)} and {len(split_elem)}."
+
+    for idx in range(len(full_elem)):
+        assert torch.all(full_elem.get_full_global_pc(idx) == split_elem.get_full_global_pc(idx))
