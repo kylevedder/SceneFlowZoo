0160ab1edc42e1a716888ece665b4e976b26ea5d
jykim <jykim94@dgist.ac.kr>
2024-04-22 18:40:26 +0000

    add config

--- configs/deflow/argo/deflow_test.py
+++ configs/deflow/argo/deflow_test.py
@@ -3,6 +3,10 @@ _base_ = "./deflow_val.py"
 has_labels = False
 
 test_dataset_root = "/efs/argoverse2/test/"
-save_output_folder = "/efs/argoverse2/test_deflow_flow/"
+save_output_folder =  "/efs/argoverse2/test_deflow_flow/"
 
-test_dataset = dict(args=dict(root_dir=test_dataset_root))
+test_dataset = dict(
+    args=dict(
+        root_dir=test_dataset_root,
+        load_flow=False,
+        eval_args=dict(output_path = "/efs/argoverse2/test_deflow_flow/")))
