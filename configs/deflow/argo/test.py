cc10f2f945a42e9036db0397fc5b081b7a3d8e5a
jykim <jykim94@dgist.ac.kr>
2024-04-22 19:49:10 +0000

    Fix the DeFlow config files

---
+++ configs/deflow/argo/test.py
@@ -0,0 +1,12 @@
+_base_ = "./train.py"
+
+has_labels = False
+
+test_dataset_root = "/efs/argoverse2/test/"
+save_output_folder = "/efs/argoverse2/test_deflow_flow/"
+
+test_dataset = dict(
+    args=dict(
+        root_dir=test_dataset_root,
+        load_flow=False,
+        eval_args=dict(output_path = save_output_folder)))
